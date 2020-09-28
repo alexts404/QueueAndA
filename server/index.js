6e9ca0ff5c38694ca8f721e6ec347386d9ba4718
alexts404 <alexts404dev@gmail.com>
2020-09-28 16:31:01 +0200

    feat: server add basic errorhandling

--- server/index.js
+++ server/index.js
@@ -18,37 +18,53 @@ io.on('connection', (socket) => {
 
   socket.on('add user', (userName, reject) => {
     const name = addUser(userName);
-    if (!name && typeof reject === 'function') {
-      reject(`User ${userName} already exists.\nChoose a different username!`);
-    } else {
+    if (name) {
       socket.userName = name;
+    } else {
+      handleError(
+        reject,
+        `User ${userName} already exists.\nChoose a different username!`,
+      );
     }
   });
 
-  socket.on('raise hand', () => {
+  socket.on('raise hand', (reject) => {
     if (enqueue(socket.userName, 'handQueue')) {
       socket.emit('broadcast queue', getQueue());
+    } else {
+      handleError(reject);
     }
   });
 
-  socket.on('raise finger', () => {
+  socket.on('raise finger', (reject) => {
     if (enqueue(socket.userName, 'fingerQueue')) {
       io.emit('broadcast queue', getQueue());
+    } else {
+      handleError(reject);
     }
   });
 
-  socket.on('lower hand', () => {
+  socket.on('lower hand', (reject) => {
     if (dequeue(socket.userName, 'handQueue')) {
       io.emit('broadcast queue', getQueue());
+    } else {
+      handleError(reject);
     }
   });
 
-  socket.on('lower finger', () => {
+  socket.on('lower finger', (reject) => {
     if (dequeue(socket.userName, 'fingerQueue')) {
       io.emit('broadcast queue', getQueue());
+    } else {
+      handleError(reject);
     }
   });
 });
+
+// error helper
+const handleError = (reject, message) => {
+  if (typeof reject === 'function') reject(message);
+};
 
 server.listen(PORT, () => {
   console.log(`🚀 Server listening at http://localhost:${PORT}`);
